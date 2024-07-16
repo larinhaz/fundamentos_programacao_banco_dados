74d32d9761f23149fed1d90d3b65fe1163aa87f2
larissa <158524041+larinhaz@users.noreply.github.com>
2024-07-16 20:59:32 -0300

    função - 16/07.

---
+++ larissa python/funcao.py
@@ -0,0 +1,31 @@
+#pre definição, uma mensagem, atividade ou rotina dentro do python, input ou if else...
+
+#def mensagem(): 
+    #print vem com uma mensagem de texto e ele auto executa por tela0
+       #print('Seja bem vindo a linguagem de programação python')
+
+#
+#mensagem ()
+#pre definição, uma mensagem, atividade ou rotina dentro do python, input ou if else...
+#def mensagem():
+#    print ('Caro cliente, por favor, não se esqueça de pagar!')
+
+#def mensagem2(nome): #definiçao
+  #  print ('Seja bem vindo' + nome + '!' )
+ 
+  #  mensagem2 ('Boboca')
+
+def mensagem(mulher):
+    print ('baixa')
+    print ('cabeluda')
+altura = str (input ('uma mulher menor que 1,60 é   '))
+cabelo = str (input ('uma mulher que não tem o cabelo pequeno é uma mulher  '))
+
+if altura <= 160:
+    print ('baixa')
+else:
+    print ('alta')
+
+
+
+
